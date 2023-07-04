b7e612f0c96c1a868b2d338f9ef1cf9d91e671d2
federico <fedeeiguer@hotmail.com>
2023-07-04 03:50:09 -0300

    agrego page santa-cruz

---
+++ pages/santa-cruz.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { GetServerSideProps } from "next";
+import Link from 'next/link';
+
+
+export default function santaCruz() {
+    return (
+        <>
+        
+        <h1>First Post Santa-Cruz</h1>;
+        
+        </>
+        );
+  }
