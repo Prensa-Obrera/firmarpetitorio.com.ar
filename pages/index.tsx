61b111d367d8392ecb62035182e2d145aaf95ef1
federico <fedeeiguer@hotmail.com>
2023-07-04 20:40:01 -0300

    agrande la letra

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,72 +4,17 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Firmarpetitorio.com.ar</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      
-        
-
-        <h1 className={styles.title}>
-          Firmarpetitorio.com.ar
-        </h1>
-
-        <p><Link href="/santa-cruz" className={styles.link}>Absolución Santa Cruz</Link></p>
-
-
-        <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+      <>
+        <Head>
+          <title>Firmarpetitorio</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Firmarpetitorio.com.ar
+          </h1>
+          <p><Link href="/santa-cruz" className={styles.link}>Absolución Santa Cruz</Link></p>
+        </main>
+      </>
+  );
 }
